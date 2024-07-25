256787fd9243d3e2f98f7d52c4be716c903dee5a
Jorgecas71 <jorgecas71@gmail.com>
2024-07-25 13:09:28 +0200

    added exercise and test

---
+++ src/main/java/dev_jorge/AppDaysMonths.java
@@ -0,0 +1,27 @@
+package dev_jorge;
+
+import java.util.Scanner;
+
+public class AppDaysMonths {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a valid month number: "); 
+        int month = scanner.nextInt();
+
+        Month monthDays = new Month(); 
+        Day days = new Day();       
+    
+        String monthName = monthDays.monthName(month);
+        int daysInMonth = days.numberOfDays(month);
+
+        System.out.println("Month " +  monthName);
+        System.out.println();      
+        System.out.println("Days: " + daysInMonth);
+
+        scanner.close();
+    }
+    
+}
+}
